3d5a0697bf33ccb752aa0464ec8435ea5720f0f6
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-14 17:03:27 -0500

    refactored seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,54 +1,44 @@
+
 const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const { User, Thought } = require('../models');
+const { getRandomName, getRandomEmail } = require('./data');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
 
-  // Drop existing courses
-  await Course.deleteMany({});
+  // Drop existing users
 
-  // Drop existing students
-  await Student.deleteMany({});
+  await User.deleteMany({});
 
-  // Create empty array to hold the students
-  const students = [];
+  // Drop existing thoughts
 
-  // Loop 20 times -- add students to the students array
+  await Thought.deleteMany({});
+
+  // Create empty array to hold the users
+
+  const users = [];
+
+  // Loop 20 times -- add users to the users array
+
   for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
+    const name = getRandomName(20);
+    const email = getRandomEmail(20);
 
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
+    users.push({
+      name,
+      email,
     });
   }
 
-  // Add students to the collection and await the results
-  // await Student.collection.insertMany(students);
-  const newStudents = await Student.insertMany(students);
+  // Add users to the collection and await the results
 
-  // Add courses to the collection and await the results
-  console.log("newStudents: ", newStudents);
+  const newUsers = await User.insertMany(users);
 
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...newStudents.map(student => student._id )],
-  });
 
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
-  console.info('Seeding complete! 🌱');
+  console.table(newUsers);
+  console.info('Successful Seeding!');
   process.exit(0);
 });
+
