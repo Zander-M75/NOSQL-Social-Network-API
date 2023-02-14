c20bcdda9015879194f6e46030a647cd1c3e02bd
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-14 16:56:30 -0500

    refactored data.js and added new data

--- utils/data.js
+++ utils/data.js
@@ -1,118 +1,61 @@
 const names = [
-  'Aaran',
-  'Aaren',
-  'Aarez',
-  'Aarman',
-  'Aaron',
-  'Aaron-James',
-  'Aarron',
-  'Aaryan',
-  'Aaryn',
-  'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Smith',
-  'Jones',
-  'Coollastname',
-  'enter_name_here',
-  'Ze',
-  'Zechariah',
-  'Zeek',
-  'Zeeshan',
-  'Zeid',
-  'Zein',
-  'Zen',
-  'Zendel',
-  'Zenith',
-  'Zennon',
-  'Zeph',
-  'Zerah',
-  'Zhen',
-  'Zhi',
-  'Zhong',
-  'Zhuo',
-  'Zi',
-  'Zidane',
-  'Zijie',
-  'Zinedine',
-  'Zion',
-  'Zishan',
-  'Ziya',
-  'Ziyaan',
-  'Zohaib',
-  'Zohair',
-  'Zoubaeir',
-  'Zubair',
-  'Zubayr',
-  'Zuriel',
-  'Xander',
-  'Jared',
-  'Courtney',
-  'Gillian',
-  'Clark',
-  'Jared',
-  'Grace',
-  'Kelsey',
-  'Tamar',
-  'Alex',
-  'Mark',
-  'Tamar',
-  'Farish',
-  'Sarah',
-  'Nathaniel',
-  'Parker',
+  "Wilson Goode",
+  "Kerrigan Clinton",
+  "Miguelangel Schmidt",
+  "Tariq Colbert",
+  "Isha Trimble",
+  "Reagan Torrez",
+  "Zaria Terry",
+  "Ricardo Harrell",
+  "August Addison",
+  "Gideon Layne",
+  "Terence Gates",
+  "Gunnar Moseley",
+  "Walker Burk",
+  "Tess Sanders",
+  "Ali Dewitt",
+  "Jacques Collins",
+  "Stephen Dobbs",
+  "Jayde Roy",
+  "Amy Atwood",
+  "Yulisa Alvarez",
+  "Kamron Mccullough",
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
+const emails = [
+  "wilson.goode@example.com",
+  "kerrigan.clinton@example.com",
+  "miguelangel.schmidt@example.com",
+  "tariq.colbert@example.com",
+  "isha.trimble@example.com",
+  "reagan.torrez@example.com",
+  "zaria.terry@example.com",
+  "ricardo.harrell@example.com",
+  "august.addison@example.com",
+  "gideon.layne@example.com",
+  "terence.gates@example.com",
+  "gunnar.moseley@example.com",
+  "walker.burk@example.com",
+  "tess.sanders@example.com",
+  "ali.dewitt@example.com",
+  "jacques.collins@example.com",
+  "stephen.dobbs@example.com",
+  "jayde.roy@example.com",
+  "amy.atwood@example.com",
+  "yulisa.alvarez@example.com"
 ];
+
+
 
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 // Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+const getRandomName = (arr) =>
+  `${getRandomArrItem(names)}`;
 
-// Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
-  const results = [];
-  for (let i = 0; i < int; i++) {
-    results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-    });
-  }
-  return results;
-};
+// Get random email
+const getRandomEmail = (arr) => `${getRandomArrItem(emails)}`;
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomName, getRandomEmail };
